9f5baad5c80f62c3df154421456934056c06de18
shimuli <shimulicedric@gmail.com>
2021-05-04 07:08:10 +0300

    redirect to page

--- models/post_new_article.php
+++ models/post_new_article.php
@@ -3,16 +3,17 @@ require 'database.php';
 $errors = [];
 $title = '';
 $content = '';
+$subfolder = "/sokosimpleblog";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $title = $_POST['title'];
-    $content =$_POST['content'];
-    
-    if ( $title == '') {
+    $content = $_POST['content'];
+
+    if ($title == '') {
         $errors[] = 'Title is required';
     }
-    if ($content  == '') {
+    if ($content == '') {
         $errors[] = 'Content is required';
     }
     if (empty($errors)) {
@@ -28,8 +29,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             mysqli_stmt_bind_param($stmt, "ss", $title, $content);
             if (mysqli_stmt_execute($stmt)) {
+
+                // redirect
+                if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+                    $protocal = 'https';
+                } else {
+                    $protocal = 'http';
+                }
+
                 $id = mysqli_insert_id($conn);
-                echo "Inserted record with ID: $id";
+                header("Location: $protocal://" . $_SERVER['HTTP_HOST'] . $subfolder . "/articles.php?id=$id");
+                exit;
+
             } else {
                 echo mysqli_stmt_error($stmt);
 
